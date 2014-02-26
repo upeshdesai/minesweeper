a99292209b41df7f6928bf967105e92aacbfd96a
acampos111 <acampos341@gmail.com>
2014-02-26 15:32:01 -0600

    moved main to minesweeper.java

---
+++ minesweeper.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class minesweeper{
+
+	public static void main (String args[]){
+			myBoard application = new myBoard();
+			application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
